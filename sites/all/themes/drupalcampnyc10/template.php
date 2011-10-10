7204e30617c6c130594d609279589032af66b1ba
Mark Reilly <mark@alienresident.net>
2011-10-10 09:35:58 -0400

    adding google verification to template.php

--- sites/all/themes/drupalcampnyc10/template.php
+++ sites/all/themes/drupalcampnyc10/template.php
@@ -1,8 +1,10 @@
 <?php
 function drupalcampnyc10_preprocess_page(&$vars, $hook) {
   // hide Flash of Unstyled Text from FF & IE 9 see http://www.extensis.com/en/WebINK/fout-b-gone/
-  if(drupal_is_front_page()) {
-    $headscript = '<script type="text/javascript">fbg.hideFOUT("asap", 400);</script>';
-    $vars['scripts'] .= $headscript;
-  }
+  //dpm($vars);
+  $googlemeta = '<meta name="google-site-verification" content="1tO-IiukYTIwsIc_oB9DmYoaI0xBPqQFVFcStDLhYoI" />';
+  $headscript = '<script type="text/javascript">fbg.hideFOUT("asap", 400);</script>';
+  $vars['head'] .= $googlemeta;
+  $vars['scripts'] .= $headscript;
 }
+
