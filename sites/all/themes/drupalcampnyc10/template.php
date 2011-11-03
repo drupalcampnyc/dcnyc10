80f35904f5aef496cdb2aa6f7697c6c861aea6cd
Mark Reilly <mark@alienresident.net>
2011-11-02 23:52:47 -0400

    changes and cleanup

--- sites/all/themes/drupalcampnyc10/template.php
+++ sites/all/themes/drupalcampnyc10/template.php
@@ -1,32 +1,27 @@
 <?php
 function drupalcampnyc10_preprocess_page(&$vars, $hook) {
-  //dpm($vars['ie8_styles']);
+  
   // Add Meta Viewport tag for mobile
-   $vars['head'] .= drupal_set_html_head("<meta name=\"viewport\" content=\"width=devide-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no\" />\r");
-   
+  $vars['head'] .= drupal_set_html_head("<meta name=\"viewport\" content=\"width=devide-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no\" />\r");
+
   // Remove the Grid
   unset($vars['setting_styles']);
-  $vars['styles'] = drupal_get_css($vars['css']);
-  
-  /* 
- 
-  
-  // Added the global style to the beginning of the conditional comments (should be in a function but TO DO)
+
+	// Added the global style to the beginning of the conditional comments (should be in a function but TO DO)
   $vars['ie8_styles_append'] = $vars['ie8_styles'];
   $vars['ie7_styles_append'] = $vars['ie7_styles'];
   $vars['ie6_styles_append'] = $vars['ie6_styles'];
+  
   unset($vars['ie8_styles'], $vars['ie7_styles'], $vars['ie6_styles']);
    
-  $vars['ie8_styles'] .= "  <link type=\"text/css\" rel=\"stylesheet\" media=\"all\" href=\"".base_path().path_to_theme()."/css/desktop-tablet-fluid.css\" />\r" . $vars['ie8_styles_append'];
-  $vars['ie7_styles'] .= "  <link type=\"text/css\" rel=\"stylesheet\" media=\"all\" href=\"".base_path().path_to_theme()."/css/desktop-tablet-fluid.css\" />\r" . $vars['ie7_styles_append'];
-  $vars['ie6_styles'] .= "  <link type=\"text/css\" rel=\"stylesheet\" media=\"all\" href=\"".base_path().path_to_theme()."/css/desktop-tablet-fluid.css\" />\r" . $vars['ie6_styles_append'];
-  */    
-   // Remove IE style sheets
-   unset($vars['ie8_styles'], $vars['ie7_styles'], $vars['ie6_styles']);  
+ 	// Remove Fusion Core IE stylesheets
+  unset($vars['ie8_styles'], $vars['ie7_styles'], $vars['ie6_styles']);  
    
-   $vars['ie8_styles'] .= "<link type=\"text/css\" rel=\"stylesheet\" media=\"all\" href=\"".base_path().path_to_theme()."/css/ie8-fixes.css\" />\r";
-   $vars['ie7_styles'] .= "<link type=\"text/css\" rel=\"stylesheet\" media=\"all\" href=\"".base_path().path_to_theme()."/css/ie7-fixes.css\" />\r";
-   $vars['ie6_styles'] .= "<link type=\"text/css\" rel=\"stylesheet\" media=\"all\" href=\"".base_path().path_to_theme()."/css/ie6-fixes.css\" />\r";
+  // Add DrupalCampNYC IE stylesheets
+  $vars['ie8_styles'] .= "<link type=\"text/css\" rel=\"stylesheet\" media=\"all\" href=\"".base_path().path_to_theme()."/css/ie8-fixes.css\" />\r";
+  $vars['ie7_styles'] .= "<link type=\"text/css\" rel=\"stylesheet\" media=\"all\" href=\"".base_path().path_to_theme()."/css/ie7-fixes.css\" />\r";
+  $vars['ie6_styles'] .= "<link type=\"text/css\" rel=\"stylesheet\" media=\"all\" href=\"".base_path().path_to_theme()."/css/ie6-fixes.css\" />\r";
+
 }
 
 function drupalcampnyc10_uc_signup_event_text($node = NULL) {
